da88aae6230734bb064f07ef5201a4f1ddb85ede
Jean-Daniel Dupas <jddupas@xooloo.com>
2021-03-15 16:50:09 +0100

    Select a couple of CRDs

--- crds.fish
+++ crds.fish
@@ -1,10 +1,27 @@
 #!/usr/local/bin/fish
 
-for crd in (kubectl get crds -o name);
-  set crd (string replace -r 'customresourcedefinition.apiextensions.k8s.io/' '' $crd)
+set CRDS certificates.cert-manager.io \
+        ciliumclusterwidenetworkpolicies.cilium.io \
+        ciliumnetworkpolicies.cilium.io \
+        podmonitors.monitoring.coreos.com \
+        probes.monitoring.coreos.com \
+        prometheusrules.monitoring.coreos.com \
+        sealedsecrets.bitnami.com \
+        servicemonitors.monitoring.coreos.com \
+        verticalpodautoscaler.autoscaling.k8s.io \
+
+
+for crd in $CRDS;
   set name (string split --max 1 '.' $crd)[1]
   echo "  Processing $crd"
   python3 kubegen.py -o k8s/{$name}.py $crd
 end
+
+# for crd in (kubectl get crds -o name);
+#   set crd (string replace -r 'customresourcedefinition.apiextensions.k8s.io/' '' $crd)
+#   set name (string split --max 1 '.' $crd)[1]
+#   echo "  Processing $crd"
+#  python3 kubegen.py -o k8s/{$name}.py $crd
+# end
 
 black k8s
