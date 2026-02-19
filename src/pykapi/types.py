aa98caa54697a8129c6f058c5796895bf93b48c7
Jean-Daniel Dupas <jddupas@xooloo.com>
2026-02-19 16:06:52 +0100

    [pykapi] Fix import with GenericType
    Generic Type may be compared to ApiType

--- src/pykapi/types.py
+++ src/pykapi/types.py
@@ -45,7 +45,9 @@ class ApiType:
         return self.version
 
     def __eq__(self, other):
-        return self.name == other.name if other else False
+        if not isinstance(other, ApiType):
+            return False
+        return self.name == other.name
 
     def __str__(self):
         return self.name
